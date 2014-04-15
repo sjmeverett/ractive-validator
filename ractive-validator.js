31434d5c5542f9e3cc9266c6a28053e790581403
stewartml <stewartml@gmail.com>
2014-04-15 19:49:02 +0100

    Initial commit.

---
+++ ractive-validator.js
@@ -0,0 +1,158 @@
+
+define(['moment'], function (moment) {
+
+  function RactiveValidator(ractive, rules, validators, messages) {
+    this.ractive = ractive;
+    this.rules = rules;
+    this._enabled = true;
+    this._validators = RactiveValidator.validators;
+    this._messages = RactiveValidator.messages;
+    this.validationErrors = {};
+
+    if (typeof validators !== 'undefined' && validators != null)
+      merge(this._validators, validators);
+
+    if (typeof messages !== 'undefined')
+      merge(this._messages, messages);
+
+    for (var k in rules) {
+      setValidator.call(this, k, rules[k]);
+    }
+  }
+
+
+  RactiveValidator.validators = {
+    required: function (value, rule) {
+      return !(typeof value === 'undefined' || value === null || value === '');
+    },
+    number: function (value, rule) {
+      return value.toString().match(/^-?[0-9]+(\.[0-9]+)?$/) != null;
+    },
+    integer: function (value, rule) {
+      return value.toString().match(/^-?[0-9]+$/) != null;
+    },
+    positive: function (value, rule) {
+      return value >= 0;
+    },
+    date: function (value, rule) {
+      return moment(value, rule).isValid() ? true : rule;
+    }
+  };
+
+
+  RactiveValidator.messages = {
+    required: 'required',
+    number: 'must be a number',
+    integer: 'must be a whole number',
+    positive: 'must be a positive number',
+    date: 'must be a date (#)'
+  };
+
+
+  RactiveValidator.prototype.enabled = function (value) {
+    this._enabled = value;
+  };
+
+
+  RactiveValidator.prototype.valid = function () {
+    if (this._enabled) {
+      validateAll.call(this);
+      return Object.keys(this.validationErrors).length == 0;
+    }
+    else {
+      return true;
+    }
+  };
+
+
+  function validateAll() {
+    for (var k in this.rules) {
+      validateKeyPath.call(this, undefined, k, this.rules[k]);
+    }
+  }
+
+
+  function validateKeyPath(parent, keypath, rules) {
+    var m = keypath.match(/^([^\*]*)\.\*(\..*)?$/);
+
+    if (m != null) {
+      var arrPath = m[1];
+      var arr = this.ractive.get(arrPath);
+
+      for (var k in arr) {
+        validateKeyPath.call(this, concat(parent, arrPath), concat(k, m[2]), rules);
+      }
+    }
+    else {
+      var keypath = concat(parent, '.', keypath);
+      var value = this.ractive.get(keypath);
+      validate.call(this, value, rules, keypath);
+    }
+  }
+
+  function concat() {
+    var str = '';
+
+    for(var i = 0; i < arguments.length; i++) {
+      if (typeof arguments[i] !== 'undefined')
+        str += arguments[i];
+    }
+
+    return str;
+  }
+
+
+  function setValidator(keypath, rules) {
+    var _this = this;
+
+    _this.ractive.observe(keypath, function (newValue, oldValue, keypath) {
+      if (!_this._enabled)
+        return;
+
+      validate.call(_this, newValue, rules, keypath);
+    }, {init: false});
+  }
+
+
+  function validate(value, rules, keypath) {
+    var error = null;
+
+    for (var k in rules) {
+      if (!this._validators.hasOwnProperty(k))
+        throw new Error('validator ' + k + ' not defined');
+
+      var result = this._validators[k].call(this, value, rules[k]);
+      var error = null;
+
+      if (typeof result === 'string') {
+        error = this._messages[k].replace('#', result);
+      }
+      else if (!result) {
+        error = this._messages[k];
+      }
+
+      if (error != null)
+        break;
+    }
+
+    if (error != null) {
+      this.ractive.set(keypath + 'Msg', error);
+      this.validationErrors[keypath] = error;
+    }
+    else {
+      this.ractive.set(keypath + 'Msg', undefined);
+      delete this.validationErrors[keypath];
+    }
+  }
+
+
+  function merge(dest, src) {
+    for (var k in src) {
+      if (src.hasOwnProperty(k))
+        dest[k] = src[k];
+    }
+  }
+
+
+  return RactiveValidator;
+});
---
+++ tests/test.js
@@ -0,0 +1,122 @@
+
+require.config({
+  baseUrl: 'jasmine',
+  shim: {
+    'jasmine': {
+      exports: 'jasmine'
+    },
+    'jasmine-html': {
+      deps: ['jasmine'],
+      exports: 'jasmine'
+    }
+  },
+  paths: {
+    'moment': '../../bower_components/moment/moment',
+    'ractive': '../../bower_components/ractive/ractive',
+    'ractive-validator': '../../ractive-validator'
+  }
+});
+
+define(['ractive-validator', 'jasmine-start', 'ractive'], function (RactiveValidator, jasmineStart, Ractive) {
+
+  describe('The built-in validators', function () {
+    var val = RactiveValidator.validators;
+
+    it('include a required validator', function () {
+      expect(val.required(undefined, true)).toEqual(false);
+      expect(val.required(null, true)).toEqual(false);
+      expect(val.required('', true)).toEqual(false);
+      expect(val.required('a value', true)).toEqual(true);
+    });
+
+    it('include a number validator', function () {
+      expect(val.number(1, true)).toEqual(true);
+      expect(val.number('100', true)).toEqual(true);
+      expect(val.number('1.52', true)).toEqual(true);
+      expect(val.number('-1.3', true)).toEqual(true);
+      expect(val.number('a', true)).toEqual(false);
+      expect(val.number('1.', true)).toEqual(false);
+      expect(val.number('1abc', true)).toEqual(false);
+    });
+
+    it('include an integer validator', function () {
+      expect(val.integer(1, true)).toEqual(true);
+      expect(val.integer('100', true)).toEqual(true);
+      expect(val.integer('-1', true)).toEqual(true);
+      expect(val.integer('a', true)).toEqual(false);
+      expect(val.integer('1.', true)).toEqual(false);
+      expect(val.integer('1abc', true)).toEqual(false);
+      expect(val.integer('1.1', true)).toEqual(false);
+    });
+
+    it('include a positive validator', function () {
+      expect(val.positive(1, true)).toEqual(true);
+      expect(val.positive('5', true)).toEqual(true);
+      expect(val.positive('0', true)).toEqual(true);
+      expect(val.positive(-1, true)).toEqual(false);
+    });
+
+    it('include a date validator', function () {
+      expect(val.date('15/04/2014', 'DD/MM/YYYY')).toEqual(true);
+      expect(val.date('1100/92/451', 'DD/MM/YYYY')).toEqual('DD/MM/YYYY');
+      expect(val.date('fish', 'DD/MM/YYYY')).toEqual('DD/MM/YYYY');
+    });
+  });
+
+
+  describe('RactiveValidator.valid', function () {
+    var html = '<input type="text" value="{{num}}"><input type="text" value="{{str}}">';
+
+    it('returns true for a valid model', function () {
+      var ractive = new Ractive({template: html, data: {num: 1, str: 'fish'}});
+      var validator = new RactiveValidator(ractive, {num: {required: true, number: true}, str: {required: true}});
+      var valid = validator.valid();
+
+      expect(valid).toEqual(true);
+    });
+
+    it('returns false for an invalid model', function () {
+      var ractive = new Ractive({el: '#el', template: html, data: {num: '', str: ''}});
+      var validator = new RactiveValidator(ractive, {num: {required: true, number: true}, str: {required: true}});
+      var valid = validator.valid();
+
+      expect(valid).toEqual(false);
+    });
+
+    it('sets error messages for an invalid model', function () {
+      var model = {num: 'a', str: ''};
+      var ractive = new Ractive({el: '#el', template: html, data: model});
+      var validator = new RactiveValidator(ractive, {num: {required: true, number: true}, str: {required: true}});
+      var valid = validator.valid();
+
+      expect(model.numMsg).toEqual(RactiveValidator.messages.number);
+      expect(model.strMsg).toEqual(RactiveValidator.messages.required);
+    });
+
+    it('handles array wildcard keypaths', function () {
+      var model = {items: [{num: 'a', str: ''}, {num: '5', str: 'a str'}]};
+      var ractive = new Ractive({el: '#el', template: html, data: model});
+      var validator = new RactiveValidator(ractive, {'items.*.num': {required: true, number: true}, 'items.*.str': {required: true}});
+      var valid = validator.valid();
+
+      expect(model.items[0].numMsg).toEqual(RactiveValidator.messages.number);
+      expect(model.items[0].strMsg).toEqual(RactiveValidator.messages.required);
+      expect(model.items[1].numMsg).toEqual(undefined);
+      expect(model.items[1].strMsg).toEqual(undefined);
+    });
+
+    it('handles object wildcard keypaths', function () {
+      var model = {items: {a: {num: 'a', str: ''}, b:{num: '5', str: 'a str'}}};
+      var ractive = new Ractive({el: '#el', template: html, data: model});
+      var validator = new RactiveValidator(ractive, {'items.*.num': {required: true, number: true}, 'items.*.str': {required: true}});
+      var valid = validator.valid();
+
+      expect(model.items.a.numMsg).toEqual(RactiveValidator.messages.number);
+      expect(model.items.a.strMsg).toEqual(RactiveValidator.messages.required);
+      expect(model.items.b.numMsg).toEqual(undefined);
+      expect(model.items.b.strMsg).toEqual(undefined);
+    })
+  });
+
+  jasmineStart();
+});
